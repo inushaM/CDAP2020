6b531b6157734522e573c288ab401724a4684275
NaduniRanasinghe <naduni.k.ranasinghe@gmail.com>
2020-04-10 12:07:48 +0530

    Initialize post pages

---
+++ e-Learning platform/Front-end/React_Frontend/src/components/CreatePost.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+class CreatePost extends Component {
+
+    constructor(pros){
+        super(pros)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <h2>New Post Page</h2>
+            </div>
+        )
+    }
+}
+
+export default CreatePost;
---
+++ e-Learning platform/Front-end/React_Frontend/src/components/EditPost.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+class EditPost extends Component {
+
+    constructor(pros){
+        super(pros)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <h2>Edit Post Page</h2>
+            </div>
+        )
+    }
+}
+
+export default EditPost;
